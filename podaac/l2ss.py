77fc6ec2b917af9dad6c8613e34a0ade337670b2
Omkar20895 <omkarreddy2008@gmail.com>
2016-12-05 00:16:39 +0530

    Updating dataset_search function

--- podaac/l2ss.py
+++ podaac/l2ss.py
@@ -1,37 +1,48 @@
-#
-#  Licensed to the Apache Software Foundation (ASF) under one or more
-#  contributor license agreements.  See the NOTICE file distributed with
-#  this work for additional information regarding copyright ownership.
-#  The ASF licenses this file to You under the Apache License, Version 2.0
-#  (the "License"); you may not use this file except in compliance with
-#  the License.  You may obtain a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
 #      http://www.apache.org/licenses/LICENSE-2.0
 #
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
-#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import requests
+from future.moves.urllib.error import HTTPError
 
 
 class L2SS:
 
     def __init__(self):
-        self.URL = 'http://podaac-tools.jpl.nasa.gov/'
+        self.URL = 'http://podaac-tools.jpl.nasa.gov/l2ss-services/l2ss/'
 
-    def dataset_search(self, dataset_id=''):
+    def dataset_search(self, dataset_id='', variable=[], sensor='', provider='', startTime='', endTime='', startIndex='', itemsPerPage=''):
         try:
-            url = self.URL + 'l2ss/dataset/search?'
-            if(dataset_id != ''):
+            url = self.URL + 'dataset/search?'
+            if(dataset_id):
                 url = url + 'datasetId=' + dataset_id
-            else:
-                raise Exception("Dataset Id is required")
+            if(variable):
+                for var in variable:
+                    url = url + '&variable=' + var
+            if(sensor):
+                url = url + '&sensor=' + sensor
+            if(provider):
+                url = url + '&provider=' + provider
+            if(startTime):
+                url = url + '&startTime=' + startTime
+            if(endTime):
+                url = url + '&endTime=' + endTime
+            if(startIndex):
+                url = url + '&startIndex=' + startIndex
+            if(itemsPerPage):
+                url = url + '&itemsPerPage=' + itemsPerPage
+
             datasets = requests.get(url)
-            if datasets.status_code == 404 or datasets.status_code == 400 or datasets.status_code == 503 or datasets.status_code == 408:
+            status_codes = [404, 400, 503, 408]
+            if datasets.status_code in status_codes:
                 datasets.raise_for_status()
 
         except requests.exceptions.HTTPError as error:
