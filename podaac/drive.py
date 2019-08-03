4b983e5d8a4ccc6976d5923af8e7df0332580efc
Lewis John McGibbney <lewis.mcgibbney@gmail.com>
2019-08-02 19:58:48 -0700

    ISSUE-140 Replace xml.etree.ElementTree.fromstring functions with their defusedxml equivalents

--- podaac/drive.py
+++ podaac/drive.py
@@ -57,11 +57,9 @@ class Drive:
         from bs4 import BeautifulSoup
         soup = BeautifulSoup(granule_search_response, 'html.parser')
         drive_list = []
-        for ftp_link in soup.find_all('link'):
-            href = ftp_link.get('href')
-            if 'ftp://podaac-ftp.jpl.nasa.gov/' in href:
-                drive_list.append(href.replace('ftp://podaac-ftp.jpl.nasa.gov', self.URL))
-            elif self.URL in href:
+        for drive_link in soup.find_all('link'):
+            href = drive_link.get('href')
+            if self.URL in href:
                 drive_list.append(href)
         return drive_list
 
