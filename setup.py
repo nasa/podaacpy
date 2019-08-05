43a56a03126a5fb3a72b0871875622a332205a16
Lewis John McGibbney <lewis.mcgibbney@gmail.com>
2019-08-05 13:50:37 -0700

    Prepare for Podaacpy 2.3.0

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ _name = 'podaacpy'
 _namespaces = []
 _test_suite = 'podaac.tests'
 _url = 'https://github.com/nasa/podaacpy'
-_version = '2.2.1'
+_version = '2.3.0'
 _zip_safe = False
 
 # Setup Metadata
